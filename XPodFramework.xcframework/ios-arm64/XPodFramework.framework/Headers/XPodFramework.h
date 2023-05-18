ec90dd0f135c87a7eb0ddcb6f17678be58e58d22
CL <cl@lbx.dev>
2023-05-18 17:13:16 +0800

    add XCFramework

---
+++ XPodFramework.xcframework/ios-arm64/XPodFramework.framework/Headers/XPodFramework.h
@@ -0,0 +1,18 @@
+//
+//  XPodFramework.h
+//  XPodFramework
+//
+//  Created by LBX-CL on 2023/5/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XPodFramework.
+FOUNDATION_EXPORT double XPodFrameworkVersionNumber;
+
+//! Project version string for XPodFramework.
+FOUNDATION_EXPORT const unsigned char XPodFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XPodFramework/PublicHeader.h>
+
+
